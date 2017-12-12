ee8a212b8c868785988f4f61244782ed51b38d5c
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 14:15:57 +0100

    ajout exo7

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>exercice 6 partie 3 php</title>
-  </head>
-  <body>
-    <?php
-    $text = 'On tient le bon bout';
-      for ($number = 1; $number <= 100; $number+=15) {
-        echo $text.'<br/>';
-      }
-     ?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>exercice 7 partie 3 php</title>
+</head>
+<body>
+  <?php
+  $age = 25;
+  $genre = 'homme';
+  function majorOrMinor($age, $genre) {
+    if ($age >= 18 && $genre == 'homme') {
+      return ('Vous êtes un homme et vous êtes majeur');
+    }
+    elseif ($age < 18 && $genre == 'homme') {
+      return ('Vous êtes un homme et vous êtes mineur');
+    }
+    elseif ($age < 18 && $genre == 'femme') {
+      return ('Vous êtes une femme et vous êtes mineur');
+    }
+    elseif ($age >= 18 && $genre == 'femme') {
+      return ('Vous êtes une femme et vous êtes majeur');
+    }
+  }
+  echo majorOrMinor($age, $genre);
+  ?>
+</body>
 </html>
